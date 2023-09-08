e0d30747756b72447a7245bc77388f62ca12f4d4
JinYoung5 <143825200+JinYoung5@users.noreply.github.com>
2023-09-08 14:51:03 +0900

    9.8

---
+++ src/kr/s19/object/array/CarMain.java
@@ -0,0 +1,29 @@
+package kr.s19.object.array;
+
+class Car{
+	//멤버 변수
+	private String color;
+	private int speed;
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+}
+
+public class CarMain {
+	public static void main(String[] args) {
+		//배열 선언 및 생성
+		Car[] carArray = new Car[5];
+		
+		
+	}
+}
