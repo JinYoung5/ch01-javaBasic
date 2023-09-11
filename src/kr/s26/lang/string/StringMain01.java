2803cc39819f893da82940c3f5e272bb8249302e
JinYoung5 <143825200+JinYoung5@users.noreply.github.com>
2023-09-11 17:32:31 +0900

    9.11

--- src/kr/s26/lang/string/StringMain01.java
+++ src/kr/s26/lang/string/StringMain01.java
@@ -3,6 +3,7 @@ package kr.s26.lang.string;
 public class StringMain01 {
 	public static void main(String[] args) {
 		//암시적으로 객체 생성
+		//암시적으로 객체를 생성할 때 문자열이 같으면 같은 객체를 공유
 		String str1 = "abc";
 		String str2 = "abc";
 	}
