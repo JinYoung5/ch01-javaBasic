57528d947987c8db86b29d7d0f6fb7dfa0a6193b
JinYoung5 <143825200+JinYoung5@users.noreply.github.com>
2023-09-11 17:44:16 +0900

    9.11

--- src/kr/s26/lang/string/StringMain01.java
+++ src/kr/s26/lang/string/StringMain01.java
@@ -27,5 +27,12 @@ public class StringMain01 {
 		//멍시적으로 객체 생성하면 같은 문자열을 사용해도 각각 새로운 객체를 생성(공유하지 않음)
 		String str3 = new String("Hello");
 		String str4 = new String("Hello");
+		
+		//객체 비교
+		if(str3 == str4) {
+			System.out.println("str3와 str4는 같은 객체");
+		}else {
+			System.out.println("str3와 str4는 다른 객체");
+		}
 	}
 }
