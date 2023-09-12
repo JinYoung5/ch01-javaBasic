d407ba63972993937856494dec29ed04958b52b9
JinYoung5 <143825200+JinYoung5@users.noreply.github.com>
2023-09-12 15:54:24 +0900

    9.12

---
+++ src/kr/s24/object2/poly/PolyMain03.java
@@ -0,0 +1,27 @@
+package kr.s24.object2.poly;
+
+//부모클래스
+class Parent2{
+	public void make() {
+		System.out.println("부모클래스의 make");
+	}
+}
+//자식클래스
+class Child2 extends Parent2{
+	@Override
+	public void make() {
+		System.out.println("자식클래스의 make:");
+	}
+}
+public class PolyMain03 {
+	public static void main(String[] args) {
+		Child2 ch = new Child2();
+		ch.make();
+		
+		System.out.println("--------------");
+		
+		Parent2 p = ch;
+		p.make();
+		
+	}
+}
